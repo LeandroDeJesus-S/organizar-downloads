bf7269ae87ac6eb81fa33fb1d6132ab59b2ca0b8
Leandro de Jesus Santos <jstleandro@gmail.com>
2022-12-05 20:01:42 -0300

    initial commit

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import os
 import shutil
 from pathlib import Path
 from collections import defaultdict
+from pprint import pprint
 
 path_downloads = Path.home() / 'Downloads'
 
-items_info = defaultdict(list)
+items_type = defaultdict(list)
 
 for item in os.listdir(path_downloads):
     path_item = os.path.join(path_downloads, item)
-    if os.path.isfile(path_item):
+    item_is_file = os.path.isfile(path_item)
+    if item_is_file:
         file_name, extension = os.path.splitext(item)
-        items_info[extension.replace('.', '')].append(item)
+        items_type[extension[1:]].append(item)
 
-for ext, files in items_info.items():
+for ext, files in items_type.items():
     os.mkdir(path_downloads / f'{ext}')
     new_dir_path = path_downloads / f'{ext}'
     for file in files:
